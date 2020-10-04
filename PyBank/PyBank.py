f2f32611d01ceac9cefbfd94e286bea78340d17e
Nick Faber <nfaber@sandiego.edu>
2020-10-04 15:33:52 -0600

    "first commit"

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -1,24 +1,60 @@
 import os
 import csv
 
-file = 'budget_data.csv'
+# create csv path
+csvpath = 'budget_data.csv'
 
-with open('budget_data.csv', newline='') as file:
-    csv_reader=csv.reader(file, delimiter=',')
+with open(csvpath) as csvfile:
 
-#budget_list = (file, "budget_data.csv")
+    csvreader = csv.reader(csvfile, delimiter=',')
 
+    print(csvfile)
 
-#for Date in "budget_data.csv":
-   # print(len("Date"))
+    # skip header
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
 
+    # variables
+    totalmonths = 0
+    row_count = 0
+    net_profitloss = 0
+    tot_profit = 0
+    profit = 0
+    tot_loss = 0
+    prev_PL = 0
+    current_PL = 0
+    tot_months = 0
+    finPL = 0
+    recent_Change = 0
+    total_Change = 0
 
+    # row count
+    for row in csvreader:
+        row_count += 1
+    csvfile.seek(0)
+    next(csvreader)
 
-
-#count=[]
-  #  monthCount=0
-   # for row in csvreader
-    #    count.append(row[])
-     #   monthCount+=1
-    #print(len(count))
-    #print(monthCount)
+    # net total P/L
+    for row in csvreader:
+        profit += int(row[1])
+        if tot_months > 1:
+            prev_PL - int(row[1])
+        else:
+            current_PL - int(row[1]) - prev_PL
+    # average change in profits / losses
+    if tot_months == 1:
+        finPL = int(row[1])
+    else:
+        recent_Change = int(row[1])-finPL
+        total_Change += recent_Change
+        finPL = int(row[1])
+    # greatest increase in profits
+    
+    # print analysis
+    print("Financial Analysis")
+    print("----------------------")
+    print(f"Total Months: {row_count}")
+    print(f"Total: ${profit}")
+    print(f"Average Change: ${finPl}")
+    print(f"Greatest Increase in Profits: ")
+    print(f"")
